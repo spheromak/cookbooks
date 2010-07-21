8904829f5bce03ada738bdfe9aead0897f69d2db
Jesse Nelson <spheromak@gmail.com>
2010-07-21 17:24:52 +0000

    adding network cbook

--- network/metadata.rb
+++ network/metadata.rb
@@ -5,4 +5,8 @@ description      "Configur Network Interfaces"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.1"
 
+%w{ redhat centos fedora xenserver }.each do |os|
+    supports os
+end
+
 
