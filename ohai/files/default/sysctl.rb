be5e864a8cc177a72a23adabaa554de0e3ff2a72
Jesse Nelson <spheromak@gmail.com>
2010-08-03 00:03:39 +0000

    sysctl.rb plugin for centos

---
+++ ohai/files/default/sysctl.rb
@@ -0,0 +1,19 @@
+# Plugin to pull sysctl data 
+provides 'sysctl'
+
+
+
+sysctl Mash.new
+
+# platform detection should go here
+# right now only centos/linux tested
+cmd = "sysctl -A"
+
+status, stdout, stderr = run_command(:command => cmd)
+return "" if stdout.nil? || stdout.empty?
+stdout.each do |line|
+  line.chomp  
+  k,v = line.strip.split(/=/) 
+  sysctl[k] = v
+end
+
