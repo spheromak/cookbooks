3ed8aca2387a382519b59b47fa875739cfb47726
Jesse Nelson <spheromak@gmail.com>
2010-08-03 19:26:26 +0000

    use more compatable  hash key? method  (broke shef on 0.9.6)
    make sure that we upate chef running config if we push a new ohai plugin

--- ohai/recipes/default.rb
+++ ohai/recipes/default.rb
@@ -8,11 +8,21 @@ directory node[:ohai][:plugin_path] do
     recursive true
 end
 
-if node.ohai.attribute?(:plugins)
+ruby_block "reload_client_config" do
+  block do
+    Chef::Config.from_file("/etc/chef/client.rb")
+  end
+    action :nothing
+end
+
+
+
+if node[:ohai].key?(:plugins)
   node[:ohai][:plugins].each do |plugin|
     remote_file node[:ohai][:plugin_path] +"/#{plugin}" do
       source plugin
       owner "chef"
+      notifies :create, resources(:ruby_block => "reload_client_config")
     end
   end
 end
