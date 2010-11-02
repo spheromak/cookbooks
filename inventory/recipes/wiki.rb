83a628ff43e6624aa5c3b01a4fa6b55354f203f5
Jesse Nelson <spheromak@gmail.com>
2010-11-02 11:34:25 -0700

    Add in sorted list of hosts by fqdn on inventory list Fix up the side panel and formatting  of the tables

--- inventory/recipes/wiki.rb
+++ inventory/recipes/wiki.rb
@@ -6,11 +6,12 @@
 #
 #
 
+include_recipe "confluence::library"
 
-out = "Generated from #{@cookbook_name} by chef contents will be overwritten\n{toc}\n"
+out = "Generated from CookBook:#{@cookbook_name} on #{node.fqdn} by chef contents will be overwritten\n{section}\n {column:width=50%}\n"
 
 
-search(:node, "dmi:system") .sort.each do |h| 
+search(:node, "dmi:system").sort {|a,b| a.fqdn <=> b.fqdn}.each do |h| 
   out << "{table-plus:#{wiki_table_attribs}}\n"
   out << "||h5. #{h[:fqdn]}||    ||\n"
   h[:dmi][:system].entries.each { |e| out << "| #{e[0]}| #{e[1]}|\n" }
@@ -19,10 +20,22 @@ search(:node, "dmi:system") .sort.each do |h|
   out << "{table-plus}\n"
 end
 
+out << "
+{column}
+
+{column:width=30%}
+
+{panel}
+{toc:style=none|indent=20px|printable=false}
+{panel}
+
+{column}
+{section}\n"
 
 wiki "Host Inventory" do
   content out
   page_id node.inventory.page_id
+  parent_id node.inventory.parent_id if node.inventory.parent_id
   url  "http://wiki.mydom.com"
   user "someuser"
   pass "somepass"
