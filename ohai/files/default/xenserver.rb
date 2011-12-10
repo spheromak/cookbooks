35a05f260e88dc17f95e546cbc5c21097060b7d1
Jesse Nelson <spheromak@gmail.com>
2011-12-10 15:26:36 -0800

    fixes for OHAI-222 ignore some pool data that solr was puking on

--- ohai/files/default/xenserver.rb
+++ ohai/files/default/xenserver.rb
@@ -1,13 +1,18 @@
+#-------------------------------------------------------------------------------
+#
 # Plugin to extend the virtualization attributes in a Xen Host
+#
+#-------------------------------------------------------------------------------
+
 require_plugin 'virtualization'
+require_plugin "#{os}::hostname"
+
 provides 'xenserver'
 
-@xe_debug=false
 
 # run std popen4 and return array of lines
 def xe_open(cmd)
   a = Array.new
-  puts "execute: #{cmd}" if @xe_debug
   popen4(cmd) do |pid, stdin, stdout, stderr|
     stdin.close
     stdout.each {|line| a << line.chomp}
@@ -19,7 +24,6 @@ end
 def xe_get_host_uuid(name) 
   xe_open("xe host-list name-label=#{name}").each do |line|
     if line =~ /uuid.*:\s(.*)$/
-      puts "got uuid #{$1}" if @xe_debug
       return  $1
     end
   end
@@ -30,7 +34,6 @@ def xe_get_resident_vms(uuid)
   vms = Array.new
   xe_open("xe vm-list resident-on=#{uuid}").each do |line|
     if line =~ /uuid.*:\s(.*)$/
-      puts "got vm: #{$1}" if @xe_debug
       vms << $1
     end
   end
@@ -38,33 +41,61 @@ def xe_get_resident_vms(uuid)
 end
 
 # return hash of param => values for vm 
-def xe_get_vm_params(uuid)
+def xe_get_params(cmd)
   h = Hash.new
-  xe_open("xe vm-param-list uuid=#{uuid}").each do |line|
+  invalid_keys = %w/last-boot-record 
+                    other-config other 
+                    software-version 
+                    other
+                    platform
+                    cpu_info 
+                    license-server 
+                    recommendations /
+
+  xe_open(cmd).each do |line|
     if line.match(/\s(.*)\s\(.*\):\s(.*)/)
       k = $1.strip
       v = $2.strip
+      k.gsub!(/{|}|<|>|\[|\]|(|)|'|"/,'')
+      next if invalid_keys.include?(k)
+      k.strip!
+      v.gsub!(/<|>|\[|\]|(|)|\'|\"/,'')
       v=v.split(/;/) if v.match(/;/)
-      v.each { |i| i.strip!}
-      puts "k-> #{k}" if @xe_debug
-      h[k] =  v
+      v.to_s.each_line do |j|
+        j.strip!
+        if j.match(/([^:]*):(.*)/)
+          # we have values that may be  foo:bar that we need to sepparate into more k/v
+          k2 = $1.strip
+	        next if invalid_keys.include?(k2)
+          v2 = $2
+          v2.strip!
+          h.merge!({ k => { k2 => v2 }})
+        else 
+          h[k] =  v
+        end
+      end
+
       h.rehash
     end
   end
   return h
 end
 
+
 if not virtualization.nil? and virtualization[:role] == 'host'
   # create a guest_list attributte listing all the guests running
-  if virtualization[:emulator] == 'xen'
+  emu = :system
+  emu = :emulator if virtualization.has_key?(:emulator) 
+  
+  if virtualization[emu] == 'xen'
     virtualization[:guests] = Mash.new
     host_uuid = xe_get_host_uuid(hostname)
     virtualization[:uuid] = host_uuid
     xe_get_resident_vms(host_uuid).each do |vm|
-      virtualization[:guests][vm] = xe_get_vm_params(vm)
+      virtualization[:guests][vm] = xe_get_params("xe vm-param-list uuid=#{vm}")
     end
-  else
-    puts "not a xen host" if @xe_debug
+    virtualization[:xenserver] = xe_get_params("xe host-param-list uuid=#{host_uuid}")
+    virtualization[:xenserver][:pool] = xe_get_params("xe  pool-list")
   end
 end
 
