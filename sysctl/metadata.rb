477cb850a9b649a57bd6261796c67a01b9f4165b
Jesse Nelson <spheromak@gmail.com>
2012-02-28 14:25:36 -0800

    cleanup readme.md for release, and first push

---
+++ sysctl/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Jesse Nelson"
+maintainer_email "spheromak@gmail.com"
+license          "Apache 2.0"
+description      "Configure/Set sysctl paramaters"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "1.0.2"
